8bb50918e279147071b7a84485fb4ba8c4d61d74
Cherinet <cheridemeke7@gmail.com>
2025-07-22 22:21:12 +0300

    index

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,53 +1,29 @@
+interface Geo {
+  lat: string;
+  lng: string;
+}
 
-export interface PostProps {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-  }
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
 
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
 
-//users interface
 export interface UserProps {
   id: number;
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
+  address: Address;
   phone: string;
   website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-} 
-
-//posts interface
-export interface PostData {
-  userId: number;
-  id?: number;
-  title: string;
-  body: string;
-}
-
-//post modal interface
-export interface PostModalProps {
-  onClose: () => void;
-  onSubmit: (post: PostData) => void;
-}
-
-//user modal interface
-export interface UserModalProps {
-  onClose: () => void;
-  onSubmit: (post: UserProps) => void;
+  company: Company;
 }
